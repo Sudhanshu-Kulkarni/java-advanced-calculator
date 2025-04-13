46b98e5dda1a6f3f4f2bd33a48c10bc2158c73b1
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-04-13 20:08:28 +0530

    Initialize Main.java class with main method structure

---
+++ Main.java
@@ -0,0 +1,15 @@
+// Name: Sudhanshu Kulkarni
+// PRN: 23070126132
+// Batch: B3
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Operations op = new Operations();
+        boolean exit = false;
+
+        
+    }
+}
